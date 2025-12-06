619d6dbf9ad72c24808c93ab88ccf5e167e6159d
tyler <ninjanarwhal07@gmail.com>
2025-12-06 14:36:07 -0500

    PID more responsive

--- src/main/java/frc/robot/subsystems/drive/Module.java
+++ src/main/java/frc/robot/subsystems/drive/Module.java
@@ -67,7 +67,7 @@ public class Module {
     // }
     driveFeedforward = new SimpleMotorFeedforward(0.1, 0.13);
         driveFeedback = new PIDController(0.05, 0.0, 0.0);
-        turnFeedback = new PIDController(1.5, 0.0, 0.0);
+        turnFeedback = new PIDController(4, 0.0, 0.0);
 
     turnFeedback.enableContinuousInput(-Math.PI, Math.PI);
     setBrakeMode(true);
